af5f552ba81de77d563cdd83e9d5fdc19d901557
paulcourts <hubertcourts@gmail.com>
2022-09-25 18:13:38 -0500

    fishinsh up

--- moment.js
+++ moment.js
@@ -28,8 +28,18 @@ $(document).ready(function () {
 
       $(".saveBtn").click(function (event) {
         event.preventDefault();
-        var value = $(this).siblings(".time-block").val();
+        var value = $(this).siblings(".time-cute").val();
         var time = $(this).parent().attr("id").split("-")[1];
         localStorage.setItem(time, value);
       });
-      
+
+ $("#hour-09 .time-cute").val(localStorage.getItem("09"));
+  $("#hour-10 .time-cute").val(localStorage.getItem("10"));
+  $("#hour-11 .time-cute").val(localStorage.getItem("11"));
+  $("#hour-12 .time-cute").val(localStorage.getItem("12"));
+  $("#hour-13 .time-cute").val(localStorage.getItem("13"));
+  $("#hour-14 .time-cute").val(localStorage.getItem("14"));
+  $("#hour-15 .time-cute").val(localStorage.getItem("15"));
+  $("#hour-16 .time-cute").val(localStorage.getItem("16"));
+  $("#hour-17 .time-cute").val(localStorage.getItem("17"));
+});
